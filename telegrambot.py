87b294db3c52204bd9da7e96de40c64a40d83662
Lukas Ruhe <lukas@uni-bremen.de>
2020-04-01 21:56:33 +0200

    server changes

--- telegrambot.py
+++ telegrambot.py
@@ -1,34 +1,42 @@
-#myapp/telegrambot.py
-# Example code for telegrambot.py module
+import io
+import cowsay
+import logging
 import telegram
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram import InlineQueryResultArticle, ParseMode, InputTextMessageContent
-from telegram.ext import CommandHandler, MessageHandler, Filters, CallbackQueryHandler, InlineQueryHandler
+import contextlib
+from telegram import InlineKeyboardButton
+from telegram import InlineKeyboardMarkup
+from telegram import InlineQueryResultArticle
+from telegram import ParseMode
+from telegram import InputTextMessageContent
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler
+from telegram.ext import Filters
+from telegram.ext import CallbackQueryHandler
+from telegram.ext import InlineQueryHandler
 from django_telegrambot.apps import DjangoTelegramBot
 from kuhsag.constants import *
-#from listerer.models import Chat, Item, Message
-
 from kuhsag.commands import *
-
-import io
-import contextlib
-import logging
-logger = logging.getLogger(__name__)
-
-import cowsay
 from uuid import uuid4
 
-def error(bot, update, error):
-    logger.error('Update "%s" caused error "%s"' % (update, error))
 
-
+logger = logging.getLogger(__name__)
 speakers = [
     {'name': 'Kuh', 'func': cowsay.cow, 'emoj': '🐄'},
     {'name': 'Tux', 'func': cowsay.tux, 'emoj': '🐧'},
 ]
 
+
+def error(bot, update, error):
+    logger.exception('Update "%s" caused error "%s"' % (update, error))
+
+
 def inlinequery(bot, update):
     query = update.inline_query.query
+    logger.debug(f'got query: {query}')
+
+    if len(query) == 0:
+        logger.info('empty query will not be processed')
+        return
     options = []
     for speaker in speakers:
         cowtext = cowify(query, func=speaker['func'])
@@ -41,13 +49,13 @@ def inlinequery(bot, update):
                 input_message_content = InputTextMessageContent(f'```{cowtext}```', parse_mode='Markdown')
             )
     )
-
+    logger.debug(f'answered with {len(options)} options')
     #results = options #todo filter by query
     update.inline_query.answer(options, cache_time=0)
 
 
 def main():
-    logger.info("Loading handlers for kuhsagbot")
+    logger.debug("Loading handlers for kuhsagbot")
     dp = DjangoTelegramBot.getDispatcher('kuhsagbot')
     for cmd in commands:
         pass_args = cmd.pass_args if hasattr(cmd, 'pass_args') else False
